d277c1d746b890a12dccd5b7d59f8eee612be449
Azim Shah <azim.1998@hotmail.com>
2024-08-09 00:11:42 +0100

    implemented first person camera movement

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { PointerLockControls } from 'three/examples/jsm/Addons.js';
 
 const properties_sidebar = document.getElementById("properties-sidebar");
 const navbar_title = document.getElementById("navbar-title");
@@ -17,6 +18,68 @@ scene.add( directionalLight );
 
 const loader = new GLTFLoader();
 
+const controls = new PointerLockControls(camera, document.body)
+
+// Add event listener to lock pointer on click
+document.addEventListener('click', () => {
+    controls.lock();
+});
+
+// Movement variables
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let velocity = new THREE.Vector3();
+let direction = new THREE.Vector3();
+const clock = new THREE.Clock();
+
+// Keydown event listener
+document.addEventListener('keydown', (event) => {
+    switch (event.code) {
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = true;
+            break;
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = true;
+            break;
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = true;
+            break;
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = true;
+            break;
+    }
+});
+
+// Keyup event listener
+document.addEventListener('keyup', (event) => {
+    switch (event.code) {
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = false;
+            break;
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = false;
+            break;
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = false;
+            break;
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = false;
+            break;
+    }
+});
+
+controls.rollSpeed = 0.05
+
 loader.load( 'models/sun/scene.gltf', function ( gltf ) {
 
 	scene.add( gltf.scene );
@@ -30,6 +93,22 @@ loader.load( 'models/sun/scene.gltf', function ( gltf ) {
 camera.position.z = 50;
 
 function animate() {
+	if (controls.isLocked) {
+        const delta = clock.getDelta();
+        velocity.x -= velocity.x * 10.0 * delta;
+        velocity.z -= velocity.z * 10.0 * delta;
+
+        direction.z = Number(moveForward) - Number(moveBackward);
+        direction.x = Number(moveRight) - Number(moveLeft);
+        direction.normalize();
+
+        if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
+        if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
+
+        controls.moveRight(-velocity.x * delta);
+        controls.moveForward(-velocity.z * delta);
+    }
+
 	renderer.render( scene, camera );
 }
 
