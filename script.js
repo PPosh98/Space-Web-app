4039f66a18440b95bc5ace808ff38d9cf12cc181
Azim Shah <azim.1998@hotmail.com>
2024-08-08 20:19:13 +0100

    add sidebar

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+const properties_sidebar = document.getElementById("properties-sidebar");
+const navbar_title = document.getElementById("navbar-title");
+const close_btn = document.getElementById("closebtn");
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
@@ -27,4 +31,15 @@ camera.position.z = 50;
 
 function animate() {
 	renderer.render( scene, camera );
+}
+
+navbar_title.addEventListener("click", openNav);
+close_btn.addEventListener("click", closeNav);
+
+function openNav() {
+    properties_sidebar.style.width = "250px";
+}
+
+function closeNav() {
+    properties_sidebar.style.width = "0px";
 }
