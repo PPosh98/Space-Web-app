5c0663d40d20361ab9dd1216ad3b3f6c98f03450
Stealthrn <niyazr2017@gmail.com>
2024-08-09 16:29:48 +0100

    loaded the sun

--- scripts/loader.js
+++ scripts/loader.js
@@ -3,10 +3,21 @@ import { scene } from './scene.js';
 
 const loader = new GLTFLoader();
 
-export function loadModels() {
-    loader.load('models/sun/scene.gltf', function (gltf) {
+function modelLoader(url, options = {}) {
+    const { scale = [1, 1, 1], position = [0, 0, 0] } = options;
+    loader.load(url, function (gltf) {
+        gltf.scene.scale.set(...scale);
+        gltf.scene.position.set(...position);
         scene.add(gltf.scene);
     }, undefined, function (error) {
         console.error(error);
+    });
+}
+export function loadModels() {
+    console.log('Hello')
+    //Loading sun
+    modelLoader('models/sun/scene.gltf', {
+        scale: [1, 1, 1],
+        position: [0, 0, 0]
     });
 }
